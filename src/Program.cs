a5ccfcd969599f06cb13ddf1d163824cc905a775
keyfkeyf <Sashapetrov200165@gmail.com>
2020-04-25 11:59:45 +0300

    edit code and add class Mass

--- src/Program.cs
+++ src/Program.cs
@@ -2,33 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace src
 {
     public class Subject
-        {
-            public string name;
-            public string surname;
-            public int semester;
-
-            public Subject(string Name, string Surname, int Semester)
-            {
-                this.name = Name;
-                this.surname = Surname;
-                this.semester = Semester;
-            }
-        }
-    class Program
     {
-       
-            static void Main(string[] args)
+        public string name;
+        public string surname;
+        public int semester;
+
+        public Subject(string Name, string Surname, int Semester)
+        {
+            this.name = Name;
+            this.surname = Surname;
+            this.semester = Semester;
+        }
+
+        public Subject()
+        {
+            this.name = "";
+            this.surname = "";
+            this.semester = 0;
+        }
+
+    }
+
+
+    public  class Mass
+    {
+      
+        public static Subject[] Create(Subject[] plan, int n)
+        {
+            //Subject[] plan = new Subject[n];
+            for (int i = 0; i < n; i++)
+            {
+                plan[i] = new Subject();
+
+                Console.WriteLine("Введите название предмета");
+                plan[i].name = Console.ReadLine();
+                //do
+                //{
+                //} while ();
+
+                Console.WriteLine("Введите фамилию преподавателя");
+                plan[i].surname = Console.ReadLine();
+                Console.WriteLine("Введите семестр");
+                plan[i].semester = Convert.ToInt32(Console.ReadLine());
+
+            }
+           
+            return plan;
+        }
+
+
+    }
+    class Program
+        {
+
+        static void Main(string[] args)
         {
             int n;
             Console.WriteLine("Введите размер массива");
             n = Convert.ToInt32(Console.ReadLine());
-            Subject[] mas = new Subject[n];
-           
+          
+          
+            Console.ReadKey();
         }
+      
+
     }
 }
